55edab3614585747fc7bd5a1addfa26fddd5f3fc
12130402 <12130402@me.mcu.edu.tw>
2024-06-27 20:00:35 +0800

    feat(product-form): 建立產品表單基本欄位定義

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../product';
+import { IProductForm } from '../interface/product-form.interface';
 
 @Component({
   selector: 'app-product-form-page',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './product-form-page.component.html',
   styleUrl: './product-form-page.component.css',
 })
-export class ProductFormPageComponent {}
+export class ProductFormPageComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  private readonly route = inject(ActivatedRoute);
+
+  form = new FormGroup<IProductForm>({
+    id: new FormControl<number | null>(null),
+    name: new FormControl<string | null>(null),
+    company: new FormControl<string | null>(null),
+    isShow: new FormControl<boolean>(false, { nonNullable: true }),
+    price: new FormControl<string | null>(null),
+  });
+
+  product!: Product;
+}
